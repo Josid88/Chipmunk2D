d1f4b8a545bda88b0dd1a4b087ed19da26040d84
JoSid88 <jorgegarciagarcia27@gmail.com>
2021-10-03 09:58:31 -0500

    Add custom layer filter functionality

--- include/chipmunk/custom/cpShapeFilter.h
+++ include/chipmunk/custom/cpShapeFilter.h
@@ -3,32 +3,49 @@
 #ifndef CHIPMUNK_SHAPE_FILTER_H
 #define CHIPMUNK_SHAPE_FILTER_H
 
-
 #include "../chipmunk.h"
+
+
+typedef struct ccpLayerFilter {
+    cpBool isActive;
+    cpBitmask currentLayers;
+    cpBitmask ignoreLayers;
+    cpBool checkAllEquals;
+    cpBool isLayerChanger;
+    cpBool isOnLayerChanger;
+    cpBitmask layerChangerKey;
+} ccpLayerFilter;
+
+#ifndef CCP_LAYER_FILTER_DEFAULT_MACRO
+#define CCP_LAYER_FILTER_DEFAULT_MACRO {cpFalse, 0, 0, cpFalse, cpFalse, cpFalse, 0}
+#endif
+static const ccpLayerFilter CCP_LAYER_FILTER_DEFAULT = CCP_LAYER_FILTER_DEFAULT_MACRO;
 
 /// Fast collision filtering type that is used to determine if two objects collide before calling collision or query callbacks.
 typedef struct cpShapeFilter {
-	/// Two objects with the same non-zero group value do not collide.
-	/// This is generally used to group objects in a composite object together to disable self collisions.
-	cpGroup group;
-	/// A bitmask of user definable categories that this object belongs to.
-	/// The category/mask combinations of both objects in a collision must agree for a collision to occur.
-	cpBitmask categories;
-	/// A bitmask of user definable category types that this object object collides with.
-	/// The category/mask combinations of both objects in a collision must agree for a collision to occur.
-	cpBitmask mask;
+    /// Two objects with the same non-zero group value do not collide.
+    /// This is generally used to group objects in a composite object together to disable self collisions.
+    cpGroup group;
+    /// A bitmask of user definable categories that this object belongs to.
+    /// The category/mask combinations of both objects in a collision must agree for a collision to occur.
+    cpBitmask categories;
+    /// A bitmask of user definable category types that this object object collides with.
+    /// The category/mask combinations of both objects in a collision must agree for a collision to occur.
+    cpBitmask mask;
+	/// Layer Filter
+    ccpLayerFilter layerFilter;
 } cpShapeFilter;
 
 /// Collision filter value for a shape that will collide with anything except CP_SHAPE_FILTER_NONE.
-static const cpShapeFilter CP_SHAPE_FILTER_ALL = {CP_NO_GROUP, CP_ALL_CATEGORIES, CP_ALL_CATEGORIES};
+static const cpShapeFilter CP_SHAPE_FILTER_ALL = {CP_NO_GROUP, CP_ALL_CATEGORIES, CP_ALL_CATEGORIES, CCP_LAYER_FILTER_DEFAULT_MACRO};
 /// Collision filter value for a shape that does not collide with anything.
-static const cpShapeFilter CP_SHAPE_FILTER_NONE = {CP_NO_GROUP, ~CP_ALL_CATEGORIES, ~CP_ALL_CATEGORIES};
+static const cpShapeFilter CP_SHAPE_FILTER_NONE = {CP_NO_GROUP, ~CP_ALL_CATEGORIES, ~CP_ALL_CATEGORIES, CCP_LAYER_FILTER_DEFAULT_MACRO};
 
 /// Create a new collision filter.
 static inline cpShapeFilter
 cpShapeFilterNew(cpGroup group, cpBitmask categories, cpBitmask mask)
 {
-	cpShapeFilter filter = {group, categories, mask};
+    cpShapeFilter filter = {group, categories, mask, CCP_LAYER_FILTER_DEFAULT};	
 	return filter;
 }
 
@@ -38,6 +55,7 @@ cpShapeFilterInit(cpShapeFilter* filter)
     filter->group = CP_NO_GROUP;
     filter->categories = CP_ALL_CATEGORIES;
     filter->mask = CP_ALL_CATEGORIES;
+    filter->layerFilter = CCP_LAYER_FILTER_DEFAULT;
 }
 
 
--- include/chipmunk/custom/cpShapeFilterReject.h
+++ include/chipmunk/custom/cpShapeFilterReject.h
@@ -2,20 +2,46 @@
 #ifndef CHIPMUNK_SHAPE_FILTER_REJECT_H
 #define CHIPMUNK_SHAPE_FILTER_REJECT_H
 
-
 #include "../chipmunk.h"
 
+
 static inline cpBool
-cpShapeFilterReject(cpShapeFilter a, cpShapeFilter b)
+cpShapeFilterReject(const cpShapeFilter a, const cpShapeFilter b)
 {
-	// Reject the collision if:
-	return (
-		// They are in the same non-zero group.
-		(a.group != 0 && a.group == b.group) ||
-		// One of the category/mask combinations fails.
-		(a.categories & b.mask) == 0 ||
-		(b.categories & a.mask) == 0
-	);
+    // Reject the collision if:
+    return (cpBool) (
+               (
+                   // They are in the same non-zero group OR...
+                   (a.group != 0 && a.group == b.group)
+                   // One of the category/mask combinations fails.
+                   || ((a.categories & b.mask) == 0 || (b.categories & a.mask) == 0)
+               ) || (
+                   // Contains layer filter and...
+                   (a.layerFilter.isActive && b.layerFilter.isActive)
+                   // Current layers are not equals and ...
+                   && (a.layerFilter.currentLayers != b.layerFilter.currentLayers)
+                   && (
+                       // check layer equality
+                       (a.layerFilter.checkAllEquals || b.layerFilter.checkAllEquals)
+                       // or not in the same layer
+                       || ((a.layerFilter.currentLayers & b.layerFilter.currentLayers) == 0)
+                       // or
+                       || (
+                           // A ignores B
+                           ((a.layerFilter.ignoreLayers & b.layerFilter.currentLayers) != 0)
+                           // And if NOT (B is layer changer and A is on layer changer and both have same keys)
+                           && !(b.layerFilter.isLayerChanger && a.layerFilter.isOnLayerChanger && ((a.layerFilter.layerChangerKey & b.layerFilter.layerChangerKey) != 0))
+                       )
+                       // or
+                       || (
+                           // B ignores A
+                           ((b.layerFilter.ignoreLayers & a.layerFilter.currentLayers) != 0)
+                           // And if NOT (A is layer changer and B is on layer changer and both have same keys
+                           && !(a.layerFilter.isLayerChanger && b.layerFilter.isOnLayerChanger && ((a.layerFilter.layerChangerKey & b.layerFilter.layerChangerKey) != 0))
+                       )
+                   )
+               )
+           );
 }
 
 
